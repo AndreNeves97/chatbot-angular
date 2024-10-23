95f2d866504417fa7ccf70faa3f47b206389f404
André Neves <andreneves3@gmail.com>
2024-10-23 01:10:15 -0300

    feat(chat message): use markdown

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MarkdownModule } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(), provideAnimationsAsync(),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    importProvidersFrom(MarkdownModule.forRoot()),
   ],
 };
--- src/app/pages/main-page/chat-page/components/chat-message/chat-message.component.ts
+++ src/app/pages/main-page/chat-page/components/chat-message/chat-message.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
+import { Message } from '../../../../../models/message.model';
 
 @Component({
   selector: 'app-chat-message',
   standalone: true,
-  imports: [],
+  imports: [MarkdownModule],
   templateUrl: './chat-message.component.html',
   styleUrl: './chat-message.component.scss',
 })
 export class ChatMessageComponent {
   @Input({ required: true })
-  received: boolean = false;
+  message!: Message;
 }
